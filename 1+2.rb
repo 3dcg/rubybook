27608cd40b061f20351b0d98541390243babbe29
Ryuichi Oosawa <3dcganimation@gmail.com>
2019-01-24 01:23:25 +0900

    計算の練習ファイル

---
+++ 1+2.rb
@@ -0,0 +1,17 @@
+puts 1 + 2
+puts 2 - 1
+puts 3 * 4
+puts 6 / 2
+puts 1+2+3*2
+puts "計算はスペース要らない"
+puts 5.0/2.0
+puts "小数点出すには小数点つけておく"
+puts 2**4
+puts "何乗する計算"
+puts 7%3
+puts "割り算の余り、何に使うの？？"
+
+puts 3.to_s+"3"
+puts "integer（整数）の3をstring（文字列）に変更してStringの3に+"
+puts 3+"3".to_i
+puts "integerの3にstringの3をintegerに変換して+"
