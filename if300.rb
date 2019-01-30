e8fe5b7de380a70d8828cc78ee0e2379b8ff7028
Ryuichi Oosawa <3dcganimation@gmail.com>
2019-01-30 23:08:19 +0900

    ifで300円持ってたらTrueみたいなやつと、その他学習

---
+++ if300.rb
@@ -0,0 +1,29 @@
+puts 1<2
+puts 1>2
+
+puts 1<=2
+puts 2>=2
+
+wallet=500
+puts wallet>=300
+#大きいならtrue
+wallet=100
+puts wallet>=300
+#大きいか同じならtrue
+puts 1==2
+puts 2==2
+puts 2==1+1
+#等しい時true
+puts 1!=2
+puts 2!=2
+#等しくないときtrue
+puts "Ruby"=="Ruby"
+puts "Ruby"!="Ruby"
+puts "Ruby"=="xxx"
+#文字も比較できるよ
+puts 2.even?
+puts 3.even?
+#偶数ならtrue
+puts 2.odd?
+puts 3.odd?
+#奇数ならtrue
