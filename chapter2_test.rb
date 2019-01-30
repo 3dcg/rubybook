88f83efa06937bfd47ee170f1c5a17aa4855b3c0
Ryuichi Oosawa <3dcganimation@gmail.com>
2019-01-30 22:36:16 +0900

    Chapter2の練習問題

---
+++ chapter2_test.rb
@@ -0,0 +1,21 @@
+puts 2+3
+puts 2.00*2.00*3.14
+puts "Ruby"
+puts "abc"+"def"
+puts "123" .to_i+"456".to_i
+
+coffee=300
+espresso=100
+puts "珈琲：#{coffee}円"
+puts "合計：#{coffee+espresso*2}円"
+coffee=400
+puts "珈琲：#{coffee}円"
+puts "合計：#{coffee+espresso*2}円"
+
+total=300*2
+p total
+tax=total*0.08
+puts "消費税#{tax}円"
+puts "合計#{total+tax}円"
+total=300*2
+puts total
